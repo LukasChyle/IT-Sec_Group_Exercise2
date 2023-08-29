02a6c60b5299a4273c86f6d6f447023f1c951877
Lukas Chyle <lukas_chyle@hotmail.com>
2023-08-29 12:41:56 +0200

    empty controller for bruteforce script

---
+++ src/main/java/example/itsec_group_exercise2/controller/BruteForceController.java
@@ -0,0 +1,20 @@
+package example.itsec_group_exercise2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bruteforce")
+public class BruteForceController {
+
+    @GetMapping
+    public String getPassword() {
+        return "password";
+    }
+
+    @GetMapping("/dictionaryattack")
+    public String getPassword2() {
+        return "password";
+    }
+}
