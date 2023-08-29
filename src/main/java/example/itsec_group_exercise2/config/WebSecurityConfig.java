0fb80f004db51b1dc60809430a51f5af070bad81
Lukas Chyle <lukas_chyle@hotmail.com>
2023-08-29 12:19:40 +0200

    SpringSec config

---
+++ src/main/java/example/itsec_group_exercise2/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package example.itsec_group_exercise2.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+       
+        http.cors(withDefaults()).csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((requests) -> requests
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                DispatcherType.ERROR).permitAll()
+                        .requestMatchers(antMatcher("/"),antMatcher( "/**")).authenticated())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
