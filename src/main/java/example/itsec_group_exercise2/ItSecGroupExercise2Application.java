05e493398e7e8105db532a1f3896936ab134a4f4
Lukas Chyle <lukas_chyle@hotmail.com>
2023-08-29 11:17:51 +0200

    Initial commit

---
+++ src/main/java/example/itsec_group_exercise2/ItSecGroupExercise2Application.java
@@ -0,0 +1,13 @@
+package example.itsec_group_exercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSecGroupExercise2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSecGroupExercise2Application.class, args);
+    }
+
+}
---
+++ src/test/java/example/itsec_group_exercise2/ItSecGroupExercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package example.itsec_group_exercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSecGroupExercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
