4755a1621adb79eef36f712edf02ed7842a7139a
Lukas Chyle <lukas_chyle@hotmail.com>
2023-08-31 10:11:21 +0200

    Bruteforce algorithm migrated to own class in services

--- src/main/java/example/itsec_group_exercise2/controller/BruteForceController.java
+++ src/main/java/example/itsec_group_exercise2/controller/BruteForceController.java
@@ -1,6 +1,7 @@
 package example.itsec_group_exercise2.controller;
 
 import example.itsec_group_exercise2.service.ConnecterToLogin;
+import example.itsec_group_exercise2.service.PasswordGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,34 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BruteForceController {
 
     ConnecterToLogin connecterToLogin;
-
     @Autowired
     public BruteForceController(ConnecterToLogin connecterToLogin) {
         this.connecterToLogin = connecterToLogin;
     }
 
-    private int tries;
-    private String response;
-    private String password;
-    private final int min = 1, max = 5; // Length of password to bruteforce
-
-    private final char[] charset = "0123456789".toCharArray();
-//    private final char[] charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-//    private final char[] charset = ("abcdefghijklmnopqrstuvwxyzAEIOU0123456789!@#" +
-//            "$%^&*()-_+=~`[]{}|:;<>,.?/BCDFGHJKLMNPQRSTVWXYZ").toCharArray();
-
     @GetMapping
     public String getPassword() {
-        tries = 0;
-        for (int length = min; length < max; length++){
-            generate("", 0, length);
-            if (password != null) {
-                System.out.println("Password is [ " + password + " ] Tried " + tries + " combinations.");
-                return "Password is [ " + password + " ] Tried " + tries + " combinations.";
-            }
-        }
-        System.out.println("Password was not found. Tried " + tries + " combinations.");
-        return  "Password was not found. Tried " + tries + " combinations.";
+
+        PasswordGenerator generator = new PasswordGenerator(connecterToLogin);
+
+        return generator.get();
     }
 
     @GetMapping("/dictionary")
@@ -48,24 +32,4 @@ public class BruteForceController {
         return "password";
     }
 
-    public void generate(String str, int pos, int length) {
-        if (length == 0) {
-            tries++;
-            System.out.println(str);
-            response = connecterToLogin.tryPasswords(str);
-            if (response.equals("http://localhost:9050/")) {
-                password = str;
-            }
-        } else {
-            if (pos != 0) {
-                pos = 0;
-            }
-            for (int i = pos; i < charset.length; i++) {
-                generate(str + charset[i], i, length - 1);
-                if (password != null) {
-                    return;
-                }
-            }
-        }
-    }
 }
---
+++ src/main/java/example/itsec_group_exercise2/service/PasswordGenerator.java
@@ -0,0 +1,54 @@
+package example.itsec_group_exercise2.service;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PasswordGenerator {
+
+    ConnecterToLogin connecterToLogin;
+
+    public PasswordGenerator(ConnecterToLogin connecterToLogin) {
+        this.connecterToLogin = connecterToLogin;
+    }
+
+    private int tries;
+    private String password;
+    private final int min = 1, max = 5; // Length of password to bruteforce
+    private final char[] charset = "0123456789".toCharArray();
+//    private final char[] charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+//    private final char[] charset = ("abcdefghijklmnopqrstuvwxyzAEIOU0123456789!@#$%^&*()-_+=~`[]{}|:;<>,.?/BCDFGHJKLMNPQRSTVWXYZ").toCharArray();
+
+    public String get() {
+        tries = 0;
+        for (int length = min; length < max; length++){
+            generate("", 0, length);
+            if (password != null) {
+                System.out.println("Password is [ " + password + " ] Tried " + tries + " combinations.");
+                return "Password is [ " + password + " ] Tried " + tries + " combinations.";
+            }
+        }
+        System.out.println("Password was not found. Tried " + tries + " combinations.");
+        return  "Password was not found. Tried " + tries + " combinations.";
+    }
+
+    private void generate(String str, int pos, int length) {
+        if (length == 0) {
+            tries++;
+            System.out.println(str);
+            String response = connecterToLogin.tryPasswords(str);
+            if (response.equals("http://localhost:9050/")) {
+                password = str;
+            }
+        } else {
+            if (pos != 0) {
+                pos = 0;
+            }
+            for (int i = pos; i < charset.length; i++) {
+                generate(str + charset[i], i, length - 1);
+                if (password != null) {
+                    return;
+                }
+            }
+        }
+    }
+}
